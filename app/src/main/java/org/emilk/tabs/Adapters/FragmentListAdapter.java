be3a9044c4d0016ee606eda2ea531f76f14232ae
emirua <emirua@hotmail.com>
2015-07-13 16:36:06 -0500

    Icon color change finally works.

--- app/src/main/java/org/emilk/tabs/Adapters/FragmentListAdapter.java
+++ app/src/main/java/org/emilk/tabs/Adapters/FragmentListAdapter.java
@@ -1,24 +1,18 @@
 package org.emilk.tabs.Adapters;
 
 import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.DrawableContainer;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.style.ImageSpan;
 
-import org.emilk.tabs.MainActivity;
+import com.astuetz.PagerSlidingTabStrip;
 
 import java.util.List;
 
 /**
  * Created by Emilio on 10/07/2015.
  */
-public class FragmentListAdapter extends FragmentPagerAdapter {
+public class FragmentListAdapter extends FragmentPagerAdapter implements PagerSlidingTabStrip.IconTabProvider {
     Context context;
     private List<Fragment> fragments;
     private List<String> titles;
@@ -43,13 +37,16 @@ public class FragmentListAdapter extends FragmentPagerAdapter {
         return fragments.size();
     }
 
+//    @Override
+//    public CharSequence getPageTitle(int position) {
+//        SpannableString spannableString = new SpannableString("  " + titles.get(position));
+//        spannableString.setSpan(new ImageSpan(context, tabIcons.get(position), ImageSpan.ALIGN_BASELINE), 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+//        return spannableString;
+//    }
+
+
     @Override
-    public CharSequence getPageTitle(int position) {
-        SpannableString spannableString = new SpannableString("  " + titles.get(position));
-        spannableString.setSpan(new ImageSpan(context, tabIcons.get(position), ImageSpan.ALIGN_BASELINE), 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-        return spannableString;
+    public int getPageIconResId(int position) {
+        return tabIcons.get(position);
     }
-
-
-
 }
--- app/src/main/java/org/emilk/tabs/MainActivity.java
+++ app/src/main/java/org/emilk/tabs/MainActivity.java
@@ -1,19 +1,22 @@
 package org.emilk.tabs;
 
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import com.astuetz.PagerSlidingTabStrip;
 
 import org.emilk.tabs.Adapters.FragmentListAdapter;
 import org.emilk.tabs.fragments.FragmentContacts;
 import org.emilk.tabs.fragments.FragmentPartidos;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     @Bind(R.id.view_pager)
     ViewPager viewPager;
     @Bind(R.id.tab_layout)
-    TabLayout tabLayout;
+    PagerSlidingTabStrip tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,15 +59,59 @@ public class MainActivity extends AppCompatActivity {
         titles.add("Partidos");
 
         List<Integer> tabicons= new ArrayList<>();
-        tabicons.add(R.drawable.abc_ic_menu_moreoverflow_mtrl_alpha);
-        tabicons.add(R.drawable.abc_ic_go_search_api_mtrl_alpha);
+        tabicons.add(R.drawable.tab_user_selector);
+        tabicons.add(R.drawable.tab_world_selector);
 
 
         FragmentListAdapter fA= new FragmentListAdapter(getSupportFragmentManager(), MainActivity.this, fragments, titles, tabicons);
 
         viewPager.setAdapter(fA);
 
-        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setViewPager(viewPager);
+
+        Field field = null;
+        try {
+            field = PagerSlidingTabStrip.class.getDeclaredField("tabsContainer");
+            field.setAccessible(true);
+            LinearLayout tabsContainer = (LinearLayout) field.get(tabLayout);
+            tabsContainer.getChildAt(0).setSelected(true);
+
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        tabLayout.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            private int currentSelection = 0;
+            Field field;
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                try {
+                    field = PagerSlidingTabStrip.class.getDeclaredField("tabsContainer");
+                    field.setAccessible(true);
+                    LinearLayout tabsContainer = (LinearLayout) field.get(tabLayout);
+                    tabsContainer.getChildAt(currentSelection).setSelected(false);
+                    currentSelection = position;
+                    tabsContainer.getChildAt(position).setSelected(true);
+                } catch (NoSuchFieldException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
     }
 
